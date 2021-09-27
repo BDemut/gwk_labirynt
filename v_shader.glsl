747ac751f0ba20a590e0603a56d4cb3469bc7dbd
Bartosz Demut <bartek.demut@wp.pl>
2021-09-27 23:33:09 +0200

    distance shader

--- v_shader.glsl
+++ v_shader.glsl
@@ -18,12 +18,16 @@ layout (location=1) in vec4 normal; //wektor normalny w wierzcholku
 out vec4 i_color;
 
 void main(void) {
+    //vec4 = P*V*vec4(0,0,0,0)
+
+    float d;
+
+    d = distance(V*M*vertex, vec4(0,0,0,1));
+    if (d < 5)
+        d = 1 - (d / 5);
+    else
+        d = 0;
+
     gl_Position=P*V*M*vertex;
-
-    mat4 G=mat4(inverse(transpose(mat3(M))));
-    vec4 n=normalize(V*G*normal);
-
-    float nl=clamp(dot(n,lightDir),0,1);
-
-    i_color=vec4(color.rgb*nl,color.a);
+    i_color=vec4(color.rgb*d,color.a);
 }
