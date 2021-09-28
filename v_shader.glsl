8a5e415bbed498dd7bc847a2715ede84381d75e8
Bartosz Demut <bartek.demut@wp.pl>
2021-09-28 19:53:51 +0200

    distance + lambert

--- v_shader.glsl
+++ v_shader.glsl
@@ -7,7 +7,6 @@ uniform mat4 M;
 
 
 uniform vec4 color=vec4(1,1,1,1);
-uniform vec4 lightDir=vec4(0,0,1,0);
 
 //Atrybuty
 layout (location=0) in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
@@ -18,16 +17,21 @@ layout (location=1) in vec4 normal; //wektor normalny w wierzcholku
 out vec4 i_color;
 
 void main(void) {
-    //vec4 = P*V*vec4(0,0,0,0)
+    vec4 lp = vec4(0,0,0, 1); //przestrzeñ oka
 
     float d;
 
-    d = distance(V*M*vertex, vec4(0,0,0,1));
+    d = distance(V*M*vertex, lp);
     if (d < 5)
         d = 1 - (d / 5);
     else
         d = 0;
 
+    vec4 l = lp - normalize(V*M*vertex);
+    vec4 n = normalize(V*M*normal);
+
+    float nl = clamp(dot(n,l),0.5,1);
+
     gl_Position=P*V*M*vertex;
-    i_color=vec4(color.rgb*d,color.a);
+    i_color=vec4(color.rgb*nl*d*d,color.a);
 }
